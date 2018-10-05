9023cc26e00fb781fa047473f848896e2d6e15aa
Bechev <bertrand.chevalier@gmail.com>
2018-10-04 21:45:22 -0400

    first commit

---
+++ src/components/Letter.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Letter extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            letterColor: '#000000'
+        }
+    }
+
+    changeColor = () => {
+        var randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+        this.setState({
+            letterColor: randomColor
+        })
+    }
+    
+    render() {
+      return (
+        <div>
+          <h1 style={{color:this.state.letterColor}} onClick={this.changeColor}>{this.props.letter}</h1>
+        </div>
+      );
+    }
+  }
+  
+  export default Letter;
+  
