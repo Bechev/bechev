5cef02b97a3d5434c5b9cfaba999a52cac2bc0f7
Bechev <bertrand.chevalier@gmail.com>
2018-10-05 07:28:34 -0400

    Add responsivness to hovering of EGCounter

--- src/components/EGCounter.js
+++ src/components/EGCounter.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
 class EGCounter extends Component {
-  render() {
-    return (
-      <div>
-          <h2>0/1</h2>
-      </div>
-    );
-  }
+
+    constructor(props){
+        super(props)
+        this.state = {
+            isHovering: false
+        }
+    }
+
+    handleMouseEnter = () => {
+        this.setState({
+            isHovering: true
+        })
+    }
+
+    handleMouseLeave = () => {
+        this.setState({
+            isHovering: false
+        })
+    }
+
+    render() {
+        const colorStyle = this.state.isHovering ? 'black' : 'red' 
+        return (
+        <div>
+            <h2 style={{color:colorStyle}} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>0/1</h2>
+        </div>
+        );
+    }
 }
 
 export default EGCounter;
